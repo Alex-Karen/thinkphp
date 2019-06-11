59c9ae63e25ed213e57ae6f4ef02d1c4844f6794
xfh <m13928810941@163.com>
2019-06-11 16:20:45 +0800

    add thinkphp

--- tp5/route/admin/admin.php
+++ tp5/route/admin/admin.php
@@ -6,17 +6,19 @@
  * Time: 13:31
  */
 use think\facade\Route;
-Route::group('admin', function() {
-    Route::get('login', '@admin/login/index')->name('admin/login/index');
-    Route::post('login', '@admin/login/loginHandler')->name('admin/login/index');
+Route::group(['prefix' => 'admin/', 'name' => 'admin'], function() {
+    Route::get('login', 'login/index')->name('admin/login/index');
+    Route::post('login', 'login/loginHandler')->name('admin/login/index');
 
-    Route::get('up', '@admin/up/index')->name('admin/up/index');
-    Route::post('upload', '@admin/up/upload')->name('admin/up/upload');
-    Route::delete('del', '@admin/up/del')->name('admin/up/del');
+    Route::get('up', 'up/index')->name('admin/up/index');
+    Route::post('upload', 'up/upload')->name('admin/up/upload');
+    Route::delete('del', 'up/del')->name('admin/up/del');
 
     Route::group(['middleware'=> ['CheckLogin']], function() {
-        Route::get('index', '@admin/index/index')->name('admin/index/index');
-        Route::get('welcome', '@admin/index/welcome')->name('admin/index/welcome');
+        Route::get('index', 'index/index')->name('admin/index/index');
+        Route::get('welcome', 'index/welcome')->name('admin/index/welcome');
+        /*资源路由*/
+        Route::resource('art', 'article');
     });
 
 });
