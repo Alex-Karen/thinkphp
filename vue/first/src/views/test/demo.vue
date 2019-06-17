551a3af2e8b1efdcbb65335a47ba7b37e9bac569
xfh <m13928810941@163.com>
2019-06-17 22:55:25 +0800

    add

--- vue/first/src/views/test/demo.vue
+++ vue/first/src/views/test/demo.vue
@@ -1,12 +1,15 @@
 <template>
-  <div></div>
+    <div>
+
+    </div>
 </template>
 
 <script>
-export default {
-    
-};
+    export default {
+        
+    }
 </script>
 
 <style scoped>
+
 </style>
