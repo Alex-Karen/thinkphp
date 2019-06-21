cecb26cd9c50ada94b7abe249487d149fa294201
xfh <m13928810941@163.com>
2019-06-21 14:19:21 +0800

    add

--- vue/leyou-manage-web/src/pages/item/Brand.vue
+++ vue/leyou-manage-web/src/pages/item/Brand.vue
@@ -70,50 +70,6 @@ export default {
           // 完成赋值后，把加载状态赋值为false
           this.loading = false;
         });
-      // // 模拟一些数据
-      // const brands = [
-      //   {
-      //     id: 2032,
-      //     name: "OPPO",
-      //     image:
-      //       "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-      //     letter: "O"
-      //   },
-      //   {
-      //     id: 2033,
-      //     name: "飞利浦（PHILIPS）",
-      //     image:
-      //       "http://img12.360buyimg.com/popshop/jfs/t18361/122/1318410299/1870/36fe70c9/5ac43a4dNa44a0ce0.jpg",
-      //     letter: "F"
-      //   },
-      //   {
-      //     id: 2034,
-      //     name: "华为（HUAWEI）",
-      //     image:
-      //       "http://img10.360buyimg.com/popshop/jfs/t5662/36/8888655583/7806/1c629c01/598033b4Nd6055897.jpg",
-      //     letter: "H"
-      //   },
-      //   {
-      //     id: 2036,
-      //     name: "酷派（Coolpad）",
-      //     image:
-      //       "http://img10.360buyimg.com/popshop/jfs/t2521/347/883897149/3732/91c917ec/5670cf96Ncffa2ae6.jpg",
-      //     letter: "K"
-      //   },
-      //   {
-      //     id: 2037,
-      //     name: "魅族（MEIZU）",
-      //     image:
-      //       "http://img13.360buyimg.com/popshop/jfs/t3511/131/31887105/4943/48f83fa9/57fdf4b8N6e95624d.jpg",
-      //     letter: "M"
-      //   }
-      // ];
-      // // 模拟延迟一段时间，随后进行赋值
-      // setTimeout(() => {
-      //   this.brands = brands;
-      //   this.totalBrands = brands.length;
-      //   this.loading = false;
-      // }, 400);
     }
   },
   mounted() {
