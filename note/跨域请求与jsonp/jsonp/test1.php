57bdbae80523b00beb9587f7df0aa0c1a8d0393e
Alex-Karen <46176533+Alex-Karen@users.noreply.github.com>
2019-06-26 17:17:03 +0800

    add

--- note/跨域请求与jsonp/jsonp/test1.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $fn = $_GET['fn'];   //callback
-    $str = 'hello world';
-
-    echo "123";  //callback('hello world')
- ?>
--- note/跨域请求与jsonp/readme.txt
+++
@@ -1,4 +0,0 @@
-如果需要部署, 只需将jsonp目录copy到phpStudy的WWW目录下
-
-并配置虚拟站点
-www.jsonp.com指向phpStudy/WWW/jsonp目录
